49a1f5f5cf54965eb7fae3bade32bb00d6250800
maurinElectroTextile <maurin@etextile.org>
2021-01-04 21:12:10 +0100

    Updated script / print out the best result

--- docs/R1_calculator.py
+++ docs/R1_calculator.py
@@ -4,17 +4,26 @@ import sys
 
 R_MIN = float(sys.argv[1])
 R_MAX = float(sys.argv[2])
-VCC = float(sys.argv[3])
+#VCC = 3.3
+VCC = 5.0
 
-for R1 in range(2, 10000, 1) :
+lastEfficiency = 0;
+
+for R1 in range(100, 10000000, 10) :
     V_min = R_MIN / (R_MIN + R1) * VCC
     V_max = R_MAX / (R_MAX + R1) * VCC
 
-    lastEfficiency = Efficiency
-    Efficiency = ((V_max - V_min) / VCC * 100)
+    efficiency = ((V_max - V_min) / VCC * 100)
+    
+    if efficiency > lastEfficiency :
+        lastEfficiency = efficiency 
+        resistor = R1
+    #print "R1 :", "%.2f" % R1,
+    #print " [ V_min", "%.2f" % V_min,
+    #print " - V_max", "%.2f" % V_max,
+    #print " - Range ", "%.2f" % (V_max - V_min),
+    #print " ] Efficiency ", "%.2f" % ((V_max - V_min) / VCC * 100)
 
-    print "R1 :", "%.2f" % R1,
-    print " [ V_min", "%.2f" % V_min,
-    print " - V_max", "%.2f" % V_max,
-    print " - Range ", "%.2f" % (V_max - V_min),
-    print " ] Efficiency ", "%.2f" % ((V_max - V_min) / VCC * 100)
+print "R1 = %d Ohm" % resistor
+print "Efficiency = %.2f %%" % lastEfficiency
+
